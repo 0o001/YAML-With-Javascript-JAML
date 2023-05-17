cdf760be81aa54eee120021463c7330a80d80815
Mustafa UZUN <mustafauzun0@gmail.com>
2023-05-17 17:10:54 +0300

    Initial commit

--- test.yaml.js
+++ test.yaml.js
@@ -1,7 +1,3 @@
-# YAML-With-Javascript-JAML
- YAML in javascript files using with labels
- 
- ```js
 /*
 multiline comment
 */
@@ -17,4 +13,3 @@ kind: //single line comment
     string: 'Hello World'
     code: console.log('hello world')
     allTypes: (/myRegex/ || 1e1 || true, false | Object & 1 + 1)
- ```
